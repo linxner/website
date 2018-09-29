6e340ce47aa589680b997d2477316059b3b105f8
linxner <342709615@qq.com>
2018-09-29 09:45:05 +0800

    more

--- src/pages/more.js
+++ src/pages/more.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 export default class Blog extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
+	
 	render() {
 		return (
 			<div className="more">
 				<h1>More</h1>
 				<div className='contract'>
 					联系方式
+				</div>
+				<div className='WeChat'>
+					微信
+				</div>
+				<div className='Email'>
+					邮箱
 				</div>
 			</div>
 		);
